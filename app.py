adccb733997e230f97738ae9e9a32bc16915b1f4
flaviasantos190885 <flaviasantos190885@gmail.com>
2025-09-21 16:08:07 -0300

    espaço

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ with st.sidebar:
 )
     st.markdown("---")
     st.info(
-        "Este painel oferece uma análise visual dos dados de violência e um módulo para estimativas futuras. "
+        "Este painel oferece uma análise visual dos dados de violência e um módulo para estimativas futuras.   "
         
         "O projeto representa o Trabalho de Conclusão de Curso (TCC) em Gestão da Tecnologia da Informação (GTI) "
         "pelo IF Sudeste MG - Campus Muriaé."
